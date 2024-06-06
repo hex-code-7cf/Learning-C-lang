0f1ebc742789fef6264e338bb9548b05bdd04ecd
hex_code_7cf <ebin19april1999@outlook.com>
2024-06-06 18:55:49 -0300

    Learning C lang Phase 1 : Variable Integer

---
+++ Phase 1/0001 Variable/integer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    number = 11;
+    printf("Value stores in variable number : %d\n",number);
+    printf("size of integer data type is : %lu\n",sizeof(number));
+    return 0;
+}
